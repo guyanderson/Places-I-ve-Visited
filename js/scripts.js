aa6936f75e55b65e20fefadb1f480328cb96e398
Guy Anderson and Jordan Loop <guy@>
2017-05-08 11:34:07 -0700

    Add Place.placeRecommend and a radio button to collect user input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
 // constructor template
-function Place(name,location,landmarks,dateVisited) {
+function Place(name,location,landmarks,dateVisited,recommend) {
   this.placeName = name;
   this.placeLocation = location;
   this.placeLandmark = landmarks;
   this.placeDateVisited = dateVisited;
+  this.placeRecommend = recommend;
 };
+
 $(document).ready(function() {
   $("form#new-place").submit(function(event) {
     event.preventDefault();
@@ -14,13 +16,15 @@ $(document).ready(function() {
     var inputtedLocation = $("input#new-location").val();
     var inputtedLandmark = $("input#new-landmark").val();
     var inputtedDate = $("input#new-date").val();
-    console.log(inputtedName, inputtedLocation, inputtedLandmark, inputtedDate);
+    var inputtedRecommendation = $("input:radio[name=radio]:checked").val();
 
     // call the constructor
-    var newPlace = new Place(inputtedName,inputtedLocation,inputtedLandmark,inputtedDate);
+    var newPlace = new Place(inputtedName,inputtedLocation,inputtedLandmark,inputtedDate, inputtedRecommendation);
+
       // append li, span, .place, and placeName
     $("ul#place").append("<li><span class = 'place'>" + newPlace.placeName + "</span></li>");
     $(".place").last().click(function() {
+
       // attach properties to the corresponding classes
       $("#show-place").show();
       $("#show-place h2").text(newPlace.placeName);
@@ -28,12 +32,15 @@ $(document).ready(function() {
       $(".place-location").text(newPlace.placeLocation);
       $(".place-landmarks").text(newPlace.placeLandmark);
       $(".place-date-visited").text(newPlace.placeDateVisited);
+      $(".place-recomendation").text(newPlace.placeRecommend);
     });
+
     // Clears the data held in the listed values
     $("input#new-name").val("");
     $("input#new-location").val("");
     $("input#new-landmark").val("");
     $("input#new-date").val("");
+
 
   });
 });
