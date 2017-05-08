0f3703b3e83569ee86719aec4ba6c32331eb41b1
Guy Anderson and Jordan Loop <jordan@>
2017-05-08 10:57:10 -0700

    append li, span, .place, and placeName. Attach properties to the corresponding classes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,10 @@
 // constructor template
-function Place(name,location,landmarks,date-visited) {
+function Place(name,location,landmarks,dateVisited) {
   this.placeName = name;
   this.placeLocation = location;
-  this.landmark = landmarks;
-  this.dateVisited = date-visited;
+  this.placeLandmark = landmarks;
+  this.placeDateVisited = dateVisited;
 };
-
 $(document).ready(function() {
   $("form#new-place").submit(function(event) {
     event.preventDefault();
@@ -15,8 +14,21 @@ $(document).ready(function() {
     var inputtedLocation = $("input#new-location").val();
     var inputtedLandmark = $("input#new-landmark").val();
     var inputtedDate = $("input#new-date").val();
+    console.log(inputtedName, inputtedLocation, inputtedLandmark, inputtedDate);
 
     // call the constructor
-    var newLocation = new Place(inputtedName,inputtedLocation,inputtedLandmark,inputtedDate)
+    var newPlace = new Place(inputtedName,inputtedLocation,inputtedLandmark,inputtedDate);
+      // append li, span, .place, and placeName
+    $("ul#place").append("<li><span class = 'place'>" + newPlace.placeName + "</span></li>");
+    $(".place").last().click(function() {
+      // attach properties to the corresponding classes
+      $("#show-place").show();
+      $("#show-place h2").text(newPlace.placeName);
+      $(".place-name").text(newPlace.placeName);
+      $(".place-location").text(newPlace.placeLocation);
+      $(".place-landmarks").text(newPlace.placeLandmark);
+      $(".place-date-visited").text(newPlace.placeDateVisited);
+    });
+
   });
 });
